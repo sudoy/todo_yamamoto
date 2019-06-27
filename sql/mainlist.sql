5be70a984556944186616a9ddac4e1432a0a223d
山本 <@TSD-YAMAMOTO>
2019-06-27 13:09:45 +0900

    変更

---
+++ sql/mainlist.sql
@@ -0,0 +1,13 @@
+create table mainlist(
+	id serial,
+	title varchar(100) not null,
+	details text,
+	value int default '1',
+	limitdate date
+);
+
+insert into mainlist(title,details,value,limitdate)
+values
+('テスト1',null,'1','2019/07/01'),
+('文字数100まで','詳細欄','3','2019/06/27'),
+('てすと2','00000000','1','2019/06/27');
